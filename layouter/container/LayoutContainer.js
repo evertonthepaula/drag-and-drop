11c5b30536134f3ce72429c8b8094d330ecc1474
evertonthepaula <evertothepaula@gmail.com>
2019-03-12 15:05:11 -0300

    Alterar Callbacks da classe de criação do layout e melhorar exemplos

--- layouter/container/LayoutContainer.js
+++ layouter/container/LayoutContainer.js
@@ -12,4 +12,16 @@ export default class LayoutContainer extends HTMLElement
 
     connectedCallback(){
     }
+
+    disconnectedCallback() {
+        console.log('Custom square element removed from page.');
+    }
+
+    adoptedCallback() {
+        console.log('Custom square element moved to new page.');
+    }
+
+    attributeChangedCallback() {
+
+    }
 }
